5e68d57ec79e0f487666195e7a378054e3132401
hareeth94 <67553835+hareeth94@users.noreply.github.com>
2021-05-01 13:33:30 +0530

    Update procfile.sh

--- procfile.sh
+++ procfile.sh
@@ -1,11 +1,2 @@
-web: sh setup.sh && streamlit run  --server.port $PORT Data_Pre_Processing.py
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
+web: sh setup.sh && streamlit run Data_Pre_Processing.py
 
