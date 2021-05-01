7b75357895ac0765ceb9cf008a6704075cf9c1cd
hareeth94 <67553835+hareeth94@users.noreply.github.com>
2021-05-01 12:38:04 +0530

    Add files via upload

---
+++ procfile.sh
@@ -0,0 +1,11 @@
+web: sh setup.sh && streamlit run  --server.port $PORT Data_Pre_Processing.py
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
+
